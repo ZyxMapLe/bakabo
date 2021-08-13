773f4423069d8dccdb26873ce3c6e504e7fcd2dc
ZyxMapLe <87278112+ZyxMapLe@users.noreply.github.com>
2021-08-13 23:52:50 +0700

    Update meme_indo.js

--- plugins/meme_indo.js
+++ plugins/meme_indo.js
@@ -1,9 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
+  let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=ramaganzapikey')
   let barbar = await url.json()
 conn.sendFile(m.chat, barbar.result, 'memeindo', JSON.stringify(barbar, null, 2), m)
 }
+handler.tags = ['sticker']
 handler.command = /^(memeindo)$/i
 handler.register = true
 handler.private = false
